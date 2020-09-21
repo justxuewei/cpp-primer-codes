34f6b94863206b2ca9f8dcbcf4267190ea2389d8
Xavier Niu <a@nxw.name>
2020-09-21 22:20:07 +0800

    sep 21, 2020

--- chapter7/Sales_data.cpp
+++ chapter7/Sales_data.cpp
@@ -12,6 +12,32 @@ struct Sales_data {
 }
 
 // nonmember Sales_data interface functions
-Sales_data add(const Sales_data&, const Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::istream &read(std::istream&, Sales_data&);
+Sales_data add(const Sales_data &, const Sales_data &);
+std::ostream &print(std::ostream &, const Sales_data &);
+std::istream &read(std::istream &, Sales_data &);
+
+double Sales_data::avg_price() const {
+    if (units_sold) {
+        return revenue / units_sold;
+    } else {
+        return 0;
+    }
+}
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+std::istream &read(std::istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item) {
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price << " ";
+    return os;
+}
